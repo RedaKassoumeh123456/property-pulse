66ceb463d736b7a4b5d2e74a62d80edda8a6a76e
RedaKassoumeh123456 <kassoumehreda@gmail.com>
2024-06-17 11:55:22 +0300

    Make Some Changes

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,7 @@
 import Hero from "@/components/Hero";
 import HomeProperties from "@/components/HomeProperties";
 import InfoBoxes from "@/components/InfoBoxes";
-const HomePage = () => {
+const HomePage = async () => {
   return (
     <>
       <Hero />
--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,7 +1,25 @@
 import PropertyCard from "@/components/PropertyCard";
-import properties from "@/properties.json";
 
-const PropertiesPage = () => {
+async function fetchProperties() {
+  try {
+    const res = await fetch(process.env.NEXT_PUBLIC_API_DOMAIN + "/properties");
+
+    if (!res.ok) {
+      throw new Error("Failed to fetch properties");
+    }
+
+    return res.json();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+const PropertiesPage = async () => {
+  const properties = await fetchProperties();
+
+  // Sort Properties By Date
+  properties.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
+
   return (
     <section className="px-4 py-6">
       {properties.length === 0 ? (
